2eab97fbceb888904f2c911eca298afbec11a1e5
JoMingyu <city7310@naver.com>
2018-05-25 10:27:55 +0900

    DONE: data class Meal

---
+++ src/datastructure/Meal.kt
@@ -0,0 +1,7 @@
+package datastructure
+
+data class Meal(
+        var breakfast: ArrayList<String> = ArrayList(),
+        var lunch: ArrayList<String> = ArrayList(),
+        var dinner: ArrayList<String> = ArrayList()
+)
